75a90981dee358b21477152bd902c366824cdaa3
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-15 16:47:23 +0200

    převod měn

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-const Rate = () => {
+const Rate = ({ from }) => {
+  const [rate, setRate] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.frankfurter.app/latest?from=${from}&to=CZK`)
+      .then((resp) => resp.json())
+      .then((json) => {
+        setRate(json.rates.CZK);
+      });
+  }, [from]);
+
   return (
     <div className="rate">
-      <div className="rate__currency">1 USD</div>
+      <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">22 CZK</div>
+      <div className="rate__value">{rate} CZK</div>
     </div>
   );
 };
